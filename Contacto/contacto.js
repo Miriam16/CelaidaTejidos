0e854f30ff70b21268f8badc3c18697fccd8d044
Miriam  Calo <meccalo@hotmail.com>
2024-10-21 11:08:49 -0300

    Add your commit

--- Contacto/contacto.js
+++ Contacto/contacto.js
@@ -1,7 +1,23 @@
+function enviar(){
+  alert('grabar');
+}
+function verificaryenviar(){
+  var nom = document.getElementById('nom').value;
+  var mail = document.getElementById('mail').value;
+  var asunto = document.getElementById('asunto').value;
+  var men = document.getElementById('men').value;
+  if(nom!='' & asunto!='' & mail!='' & men!=''){
+    enviar();
+  }
+  else{
+    alert('Por favor completar los Datos solicitados');
+    document.getElementById('nom').focus();
+  }
+}
 function abriryoutube(){
-  alert('Hola');
+  window.open('https://youtube.com/@celaida.tejidos?si=eVjjuCvdzKPyztkc');
 }
 window.onload = function () {
-    //document.getElementById('canalyoutube').addEventListener('click', abriryoutube);
     document.getElementById('canalyoutube').onclick = abriryoutube
+    document.getElementById('enviar').onclick = verificaryenviar
 }
